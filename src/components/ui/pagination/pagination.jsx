724351527a30eb282c6ca2d20a74947c85418722
AnnaRodionova <anna9grace@gmail.com>
2021-12-06 17:52:57 +0300

    realize pagination's functionality

---
+++ src/components/ui/pagination/pagination.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Button from '../button/button';
+import styles from './pagination.module.scss';
+import { MAX_PRODUCTS_SHOWN } from '../../../const';
+
+const FIRST_PAGE = 1;
+
+function Pagination({guitarsCount, currentPage, clickHandler}) {
+  const totalPages = Math.ceil(guitarsCount / MAX_PRODUCTS_SHOWN);
+  const pages = new Array(totalPages).fill().map((page, index) => index + 1);
+  const isFirstPageActive = currentPage === FIRST_PAGE;
+  const isLastPageActive = currentPage === totalPages;
+
+  const renderFullPagination = () => (
+    pages.map((page) => (
+      <Button
+        key={page}
+        data-page-number={page}
+        className={classNames(styles.button, page === currentPage && styles.active)}
+        onClick={(evt) => clickHandler(evt.target.textContent)}
+        disabled={page === currentPage}
+      >
+        {page}
+      </Button>
+    ))
+  );
+
+
+  return (
+    <div className={styles.wrapper}>
+      {totalPages > 1 &&
+        <Button
+          className={classNames(styles.button, styles.first, isFirstPageActive && 'hidden')}
+          onClick={() => clickHandler(currentPage - 1)}
+        >
+          Назад
+        </Button>}
+
+      {renderFullPagination()}
+
+      {totalPages > 1 &&
+        <Button
+          className={classNames(styles.button, styles.last, isLastPageActive && 'hidden')}
+          onClick={() => clickHandler(currentPage + 1)}
+        >
+          Далее
+        </Button>}
+    </div>
+  );
+}
+
+Pagination.propTypes = {
+  guitarsCount: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
+
+export default Pagination;
--- src/components/ui/products-list/products-list.jsx
+++ src/components/ui/products-list/products-list.jsx
@@ -4,28 +4,41 @@ import styles from './product-list.module.scss';
 import { getFilteredGuitars } from '../../../store/data-slice/selectors';
 import { useSelector } from 'react-redux';
 import { MAX_PRODUCTS_SHOWN } from '../../../const';
+import Pagination from '../pagination/pagination';
+
+const getGuitarsToShow = (index, guitars) => guitars.slice(index, Math.min(guitars.length, index + MAX_PRODUCTS_SHOWN));
 
 function ProductList() {
   const guitars = useSelector(getFilteredGuitars);
+  const [currentPage, setCurrentPage] = useState(1);
   const [startIndex, setStartIndex] = useState(0);
-  console.log(guitars);
+  let guitarsToShow = getGuitarsToShow(startIndex, guitars);
 
   useEffect(() => {
+    setCurrentPage(1);
     setStartIndex(0);
   }, [guitars]);
 
-  // const onShowMoreClick = () => dispatch(getGuitarsRenderedCount());
+  useEffect(() => {
+    guitarsToShow = getGuitarsToShow(startIndex, guitars);
+  }, [startIndex]);
 
-  const guitarsToShow = guitars.slice(startIndex, Math.min(guitars.length, startIndex + MAX_PRODUCTS_SHOWN));
+  const switchPageHandler = (page) => {
+    setCurrentPage(+page);
+    setStartIndex(+page * MAX_PRODUCTS_SHOWN - MAX_PRODUCTS_SHOWN);
+  };
 
   return (
-    <ul className={styles.list}>
-      {guitarsToShow.map((item) => (
-        <li key={item.id} className={styles.item}>
-          <ProductCard guitarData={item}/>
-        </li>
-      ))}
-    </ul>
+    <React.Fragment>
+      <ul className={styles.list}>
+        {guitarsToShow.map((item) => (
+          <li key={item.id} className={styles.item}>
+            <ProductCard guitarData={item}/>
+          </li>
+        ))}
+      </ul>
+      <Pagination guitarsCount={guitars.length} currentPage={currentPage} clickHandler={switchPageHandler}/>
+    </React.Fragment>
   );
 }
 
